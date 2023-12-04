e6a329486486bba84e8a1ada78337bfd1d8c9b4a
Kowiy Gidado <gida0001@algonquinlive.com>
2023-12-04 00:54:16 -0500

    step 1

--- app/src/main/java/com/cst2335/kowiy/MainActivity.java
+++ app/src/main/java/com/cst2335/kowiy/MainActivity.java
@@ -1,14 +1,47 @@
 package com.cst2335.kowiy;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linear);
+
+        // Click listener for "Click Here" Button
+        Button clickHereButton = findViewById(R.id.button);
+        clickHereButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Show a Toast message
+                Toast.makeText(MainActivity.this, getResources().getString(R.string.toast_message), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Checked change listener for Switch
+        Switch switchButton = findViewById(R.id.switch);
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                // Show a Snackbar with the current switch state
+                String switchState = isChecked ? "on" : "off";
+                Snackbar.make(compoundButton, "The switch is now " + switchState, Snackbar.LENGTH_LONG)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                // Undo action: Set the switch back to its original state
+                                switchButton.setChecked(!isChecked);
+                            }
+                        })
+                        .show();
+            }
+        });
     }
-}
