0a2bc8d800530c25f3cc831b9480f917461e00e1
Kowiy Gidado <gida0001@algonquinlive.com>
2023-12-04 09:17:36 -0500

    step 3

--- app/src/main/java/com/cst2335/kowiy/MainActivity.java
+++ app/src/main/java/com/cst2335/kowiy/MainActivity.java
@@ -12,10 +12,9 @@ import com.google.android.material.snackbar.Snackbar;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.activity_main_relative);
 
         // Click listener for "Click Here" Button
         Button clickHereButton = findViewById(R.id.button);
